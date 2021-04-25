6907b2bdc08984ace5504a9ca27adcdeb83dbff9
juanmaluzzi <juan.luzzi@davinci.edu.ar>
2021-04-25 18:35:08 -0300

    asd
    Signed-off-by: juanmaluzzi <juan.luzzi@davinci.edu.ar>

--- Program.cs
+++ Program.cs
@@ -24,43 +24,70 @@ namespace TP1
                 switch (choose)
                         {
                             case 1: 
-                            Console.WriteLine("Ingrese codigo de cabaña");
-                            int codigoCab = int.Parse(Console.ReadLine());
-                            Console.WriteLine("Ingrese nombre de cabaña");
-                            string nombreCab = Console.ReadLine();
-                            Console.WriteLine("Ingrese ciudad de cabaña");
-                            string ciudadCab = Console.ReadLine();
-                            Console.WriteLine("Ingrese barrio de cabaña");
-                            string barrioCab = Console.ReadLine();
-                            Console.WriteLine("Ingrese estrellas de cabaña");
-                            int estrellasCab = int.Parse(Console.ReadLine());
-                            Console.WriteLine("Ingrese cantidad de Personas");
-                            int cantPersonasCab = int.Parse(Console.ReadLine());
-                            Console.WriteLine("¿Tiene TV?");
-                            bool tieneTv = bool.Parse(Console.ReadLine());
-                            Console.WriteLine("Precio por dia");
-                            float precioDia = float.Parse(Console.ReadLine());
-                            Console.WriteLine("Ingrese cantidad de habitaciones");
-                            int cantHabitaciones = int.Parse(Console.ReadLine());
-                            Console.WriteLine("Ingrese cantidad de baños");
-                            int cantBanos = int.Parse(Console.ReadLine());
-                            //Guarda el array con los valores pasados por consola
-                            agencia1.insertarAlojamiento(new Cabaña(codigoCab, nombreCab, ciudadCab,
-                            barrioCab, estrellasCab, cantPersonasCab, tieneTv, precioDia, cantHabitaciones, cantBanos));
-                            //imprime el array 
-                            Console.WriteLine("Se ha ingresado el siguiente alojamiento: ");
-                            foreach (Alojamiento a in agencia1.getAlojamientos()) 
-                                {
-                                Console.WriteLine(a.ToString());                                                                
-                                }     
+                                Console.WriteLine("Ingrese codigo de cabaña");
+                                int codigoCab = int.Parse(Console.ReadLine());
+                                Console.WriteLine("Ingrese nombre de cabaña");
+                                string nombreCab = Console.ReadLine();
+                                Console.WriteLine("Ingrese ciudad de cabaña");
+                                string ciudadCab = Console.ReadLine();
+                                Console.WriteLine("Ingrese barrio de cabaña");
+                                string barrioCab = Console.ReadLine();
+                                Console.WriteLine("Ingrese estrellas de cabaña");
+                                int estrellasCab = int.Parse(Console.ReadLine());
+                                Console.WriteLine("Ingrese cantidad de Personas");
+                                int cantPersonasCab = int.Parse(Console.ReadLine());
+                                Console.WriteLine("¿Tiene TV?");
+                                bool tieneTv = bool.Parse(Console.ReadLine());
+                                Console.WriteLine("Precio por dia");
+                                float precioDia = float.Parse(Console.ReadLine());
+                                Console.WriteLine("Ingrese cantidad de habitaciones");
+                                int cantHabitaciones = int.Parse(Console.ReadLine());
+                                Console.WriteLine("Ingrese cantidad de baños");
+                                int cantBanos = int.Parse(Console.ReadLine());
+                                //Guarda el array con los valores pasados por consola
+                                agencia1.insertarAlojamiento(new Cabaña(codigoCab, nombreCab, ciudadCab,
+                                barrioCab, estrellasCab, cantPersonasCab, tieneTv, precioDia, cantHabitaciones, cantBanos));
+                                //imprime el array 
+                                Console.WriteLine("Se ha ingresado la siguiente Cabaña: ");
+                                foreach (Alojamiento a in agencia1.getAlojamientos()) 
+                                    {
+                                    Console.WriteLine(a.ToString());                                                                
+                                    }     
                             break;      
                             
                             case 2:
-                                 Console.WriteLine("asd");
+                                Console.WriteLine("Ingrese codigo de hotel");
+                                int codigoHotel = int.Parse(Console.ReadLine());
+                                Console.WriteLine("Ingrese nombre de hotel");
+                                string nombreHotel = Console.ReadLine();
+                                Console.WriteLine("Ingrese ciudad de hotel");
+                                string ciudadHotel = Console.ReadLine();
+                                Console.WriteLine("Ingrese barrio de hotel");
+                                string barrioHotel = Console.ReadLine();
+                                Console.WriteLine("Ingrese estrellas de hotel");
+                                int estrellasHotel = int.Parse(Console.ReadLine());
+                                Console.WriteLine("Ingrese cantidad de Personas");
+                                int cantPersonasHotel = int.Parse(Console.ReadLine());
+                                Console.WriteLine("¿Tiene TV? Ingrese true o false");
+                                bool tieneTvHotel = bool.Parse(Console.ReadLine());
+                                Console.WriteLine("Precio por dia");
+                                float precioDiaHotel = float.Parse(Console.ReadLine()); 
+                                agencia1.insertarAlojamiento(new Hotel(codigoHotel, nombreHotel, ciudadHotel,
+                                barrioHotel, estrellasHotel, cantPersonasHotel, tieneTvHotel, precioDiaHotel));
+                                //imprime el array 
+                                Console.WriteLine("Se ha ingresado el siguiente Hotel: ");
+                                foreach (Alojamiento a in agencia1.getAlojamientos()) 
+                                    {
+                                    Console.WriteLine(a.ToString());                                                                
+                                    }     
+                            break;
+
+                            case 3:
+                                Console.Clear();
                             break;
 
                             default:
-                                 Console.WriteLine("Default case");
+                                Console.WriteLine("Ingrese un número válido");
                             break;
 
                          }
